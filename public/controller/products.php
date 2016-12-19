521bafd9f554e7e0347084870ea234210b372df3
dione <dione@tagcash.com>
2016-12-19 11:45:23 +0800

    added manufacturer on response

--- public/controller/products.php
+++ public/controller/products.php
@@ -2,15 +2,16 @@
 
 	$app->post('/product/list', function ($request, $response, $args) {
 		$conditions = array('conditions' => array('available = ?', 1));
-		if ($request->getParsedBody()['manufacturer_id']) {
-			$conditions = array('conditions' => array('available = ? and manufacturer_id = ?', 1, $request->getParsedBody()['manufacturer_id']));
-		} 
+		// if ($request->getParsedBody()['manufacturer_id']) {
+		// 	$conditions = array('conditions' => array('available = ? and manufacturer_id = ?', 1, $request->getParsedBody()['manufacturer_id']));
+		// } 
 		$phoneArray = array();
 		foreach (Product::all($conditions) as $result) {
 			$specs = Specification::find($result->id);
-			$res['phone'] = $result->to_array(); 
+			$res['phone'] = $result->to_array();  
+			$res['phone']['manufacturer'] = Manufacturer::find($res['phone']['manufacturer_id'])->to_array()['code'];
 			$res['specs'] = $specs->to_array();
 			array_push($phoneArray, $res); 
 		}
-		echo json_encode(array('result'=>$phoneArray));
+		return json_encode(array('result'=>$phoneArray));
 	}); 
